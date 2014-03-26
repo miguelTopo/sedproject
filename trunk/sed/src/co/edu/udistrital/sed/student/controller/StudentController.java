e2075e81dc8a5fe9c523a502045ad55f9dfae208
migueltrock@gmail.com <migueltrock@gmail.com@7f685e8f-f732-441d-a52b-f1eafe9f0095>
2014-03-26 03:14:37 +0000

    Listando estudiantes

--- trunk/sed/src/co/edu/udistrital/sed/student/controller/StudentController.java
+++ trunk/sed/src/co/edu/udistrital/sed/student/controller/StudentController.java
@@ -1,11 +1,14 @@
 package co.edu.udistrital.sed.student.controller;
 
+import java.util.List;
+
 import org.hibernate.Transaction;
 
+import co.edu.udistrital.core.common.controller.Controller;
 import co.edu.udistrital.sed.model.Student;
 import co.edu.udistrital.sed.student.model.StudentDAO;
 
-public class StudentController {
+public class StudentController extends Controller{
 
 	// ////Maneja transacciones,
 
@@ -14,8 +17,8 @@ public class StudentController {
 		Transaction tx = null;
 		try {
 			tx = dao.getSession().beginTransaction();
-//			dao.getSession().save(new Student());
-//			tx.commit();
+			// dao.getSession().save(new Student());
+			// tx.commit();
 			return dao.loadStudent(idStudent);
 		} catch (Exception e) {
 			dao.getSession().cancelQuery();
@@ -26,6 +29,27 @@ public class StudentController {
 			dao = null;
 			tx = null;
 		}
+	}
+
+	/** @author PPaez */
+	public List<Student> loadStudentList(Long grade, Long course)
+			throws Exception {
+		StudentDAO dao = new StudentDAO();
+		Transaction tx = null;
+		try {
+			System.out.print("Controller");
+			tx = dao.getSession().beginTransaction();
+			return dao.loadStudentList(grade, course);
+		} catch (Exception e) {
+			dao.getSession().cancelQuery();
+			tx.rollback();
+			throw e;
+		} finally {
+			dao.getSession().close();
+			dao = null;
+			tx = null;
+		}
+
 	}
 
 }
--- trunk/sed/src/co/edu/udistrital/sed/student/model/StudentDAO.java
+++ trunk/sed/src/co/edu/udistrital/sed/student/model/StudentDAO.java
@@ -1,5 +1,6 @@
 package co.edu.udistrital.sed.student.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.hibernate.Query;
@@ -9,169 +10,171 @@ import co.edu.udistrital.core.common.controller.IState;
 import co.edu.udistrital.core.connection.HibernateDAO;
 import co.edu.udistrital.sed.model.Student;
 
-public class StudentDAO extends HibernateDAO{
+public class StudentDAO extends HibernateDAO {
 
 	public Student loadStudent(Long idStudent) throws Exception {
 		StringBuilder hql = null;
 		Query qo = null;
 		try {
-		hql.append(" SELECT s.id AS id, "); 
-		hql.append(" s.identification AS identification, "); 
-		hql.append(" s.name AS name  ");
-		hql.append(" FROM Student s ");
-		hql.append(" WHERE s.id = :idloquesea ");
-		hql.append(" AND s.state = :state ");
-		
-		qo = getSession().createQuery(hql.toString()).setResultTransformer(Transformers.aliasToBean(Student.class));
-		qo.setParameter("idloquesea", idStudent);
-		qo.setParameter("state", IState.ACTIVE);
-		qo.setMaxResults(1);	
-		
-		return (Student) qo.uniqueResult();
+			hql.append(" SELECT s.id AS id, ");
+			hql.append(" s.identification AS identification, ");
+			hql.append(" s.name AS name  ");
+			hql.append(" FROM Student s ");
+			hql.append(" WHERE s.id = :idloquesea ");
+			hql.append(" AND s.state = :state ");
+
+			qo = getSession().createQuery(hql.toString()).setResultTransformer(
+					Transformers.aliasToBean(Student.class));
+			qo.setParameter("idloquesea", idStudent);
+			qo.setParameter("state", IState.ACTIVE);
+			qo.setMaxResults(1);
+
+			return (Student) qo.uniqueResult();
 		} catch (Exception e) {
 			throw e;
-		}finally{
+		} finally {
 			hql = null;
 			qo = null;
 		}
 	}
-	
-	public List<Student> loadListStudentByCourse (Long idCourse) throws Exception {
-		StringBuilder hql = null;
-		Query qo = null;
-		try {
-			
-			
-			hql.append("select s.id AS id,"); 
-			hql.append("s.identification AS identificacion,"); 
-			hql.append("s.name AS name");
-			hql.append("s.idCourse AS idCourse");
-			hql.append("FROM student s");
-			hql.append("WHERE s.idCourse=:idCourse");
-			hql.append("AND s.state=:paramState");
-			
-		qo = getSession().createQuery(hql.toString()).setResultTransformer(Transformers.aliasToBean(Student.class));
-		qo.setParameter("paramState", IState.ACTIVE);
-		qo.setParameter("idCourse", idCourse);
-			
-		
-		return qo.list();
-		} catch (Exception e) {
-			throw e;
-		}finally{
-			hql = null;
-			qo = null;
-		}
-	}
-	
-	
-	
-	public List<Student> loadListStudentByGrade (Long idGrade) throws Exception {
+
+	public List<Student> loadListStudentByCourse(Long idCourse)
+			throws Exception {
 		StringBuilder hql = null;
 		Query qo = null;
 		try {
 
 			hql.append("select s.id AS id,");
 			hql.append("s.identification AS identificacion,");
-			hql.append("s.name AS name,");
-			hql.append("s.idCourse as idCourse,");
-			hql.append("c.idgrade as idgrade");
-			hql.append("FROM student s JOIN course c");
-			hql.append("ON s.idCourse=:c.id");
-			hql.append("WHERE c.idgrade=:idGrade");
-			hql.append("AND s.state=:1;");
-			
-		qo = getSession().createQuery(hql.toString()).setResultTransformer(Transformers.aliasToBean(Student.class));
-		qo.setParameter("paramState", IState.ACTIVE);
-		qo.setParameter("idGrade", idGrade);
-			
-		
-		return qo.list();
+			hql.append("s.name AS name");
+			hql.append("s.idCourse AS idCourse");
+			hql.append("FROM student s");
+			hql.append("WHERE s.idCourse=:idCourse");
+			hql.append("AND s.state=:paramState");
+
+			qo = getSession().createQuery(hql.toString()).setResultTransformer(
+					Transformers.aliasToBean(Student.class));
+			qo.setParameter("paramState", IState.ACTIVE);
+			qo.setParameter("idCourse", idCourse);
+
+			return qo.list();
 		} catch (Exception e) {
 			throw e;
-		}finally{
+		} finally {
 			hql = null;
 			qo = null;
 		}
 	}
-	
+
+	public List<Student> loadStudentList(Long idGrade, Long idCourse)
+			throws Exception {
+		StringBuilder hql = null;
+		Query qo = null;
+
+		try {
+			hql = new StringBuilder();
+			hql.append(" SELECT s.id AS id, ");
+			hql.append(" s.identification AS identification, ");
+			hql.append(" s.name AS name, ");
+			hql.append(" s.idCourse AS idCourse, ");
+			hql.append(" c.idGrade AS idGrade, ");
+			hql.append(" c.name AS courseName ");
+			hql.append(" FROM Student s, Course c ");
+			hql.append(" WHERE s.idCourse = c.id  ");
+			hql.append(" AND c.idGrade = :idGrade ");
+			hql.append(" AND s.idCourse = :idCourse ");
+			hql.append(" AND s.state = :state ");
+			hql.append(" AND c.state = :state ");
+
+			qo = getSession().createQuery(hql.toString()).setResultTransformer(
+					Transformers.aliasToBean(Student.class));
+			qo.setParameter("idGrade", idGrade);
+			qo.setParameter("idCourse", idCourse);
+			qo.setParameter("state", IState.ACTIVE);
+			List<Student> lista = qo.list();
+
+			return lista;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			hql = null;
+			qo = null;
+		}
+	}
 
 	public boolean deleteStudent(long idStudent) throws Exception {
 		StringBuilder hql = null;
 		Query qo = null;
 		try {
-		hql.append(" UPDATE Student"); 
-		hql.append(" SET state=0"); 
-		hql.append(" WHERE id=:idStudent ");
-		hql.append(" AND s.state=:state ");
-		
-		qo = getSession().createQuery(hql.toString()).setResultTransformer(Transformers.aliasToBean(Student.class));
-		qo.setParameter("state", IState.ACTIVE);
-		qo.setParameter("paramId", idStudent);
-		
-		qo.executeUpdate();
-		return true;
-		
+			hql.append(" UPDATE Student");
+			hql.append(" SET state=0");
+			hql.append(" WHERE id=:idStudent ");
+			hql.append(" AND s.state=:state ");
+
+			qo = getSession().createQuery(hql.toString()).setResultTransformer(
+					Transformers.aliasToBean(Student.class));
+			qo.setParameter("state", IState.ACTIVE);
+			qo.setParameter("paramId", idStudent);
+
+			qo.executeUpdate();
+			return true;
+
 		} catch (Exception e) {
 			throw e;
-		}finally{
+		} finally {
 			hql = null;
 			qo = null;
 		}
 	}
 
-	/*public boolean createStudent() throws Exception {
+	/*
+	 * public boolean createStudent() throws Exception { StringBuilder hql =
+	 * null; Query qo = null; try { hql.append(" UPDATE Student");
+	 * hql.append(" SET state=0"); hql.append(" WHERE id:paramId ");
+	 * hql.append(" AND s.state = :paramState ");
+	 * 
+	 * qo =
+	 * getSession().createQuery(hql.toString()).setResultTransformer(Transformers
+	 * .aliasToBean(Student.class)); qo.setParameter("state", IState.ACTIVE); //
+	 * qo.setParameter("paramId", idStudent);
+	 * 
+	 * qo.executeUpdate(); return true;
+	 * 
+	 * } catch (Exception e) { throw e; }finally{ hql = null; qo = null; } }
+	 */
+	public boolean updateStudent(Long idStudent, String identificacionStudent,
+			String nameStudent, Long idCourse) throws Exception {
 		StringBuilder hql = null;
 		Query qo = null;
 		try {
-		hql.append(" UPDATE Student"); 
-		hql.append(" SET state=0"); 
-		hql.append(" WHERE id:paramId ");
-		hql.append(" AND s.state = :paramState ");
-		
-		qo = getSession().createQuery(hql.toString()).setResultTransformer(Transformers.aliasToBean(Student.class));
-		qo.setParameter("state", IState.ACTIVE);
-//		qo.setParameter("paramId", idStudent);
-		
-		qo.executeUpdate();
-		return true;
-		
-		} catch (Exception e) {
-			throw e;
-		}finally{
-			hql = null;
-			qo = null;
-		}
-	}*/
-		public boolean updateStudent(Long idStudent, String identificacionStudent, String nameStudent, Long idCourse) throws Exception {
-			StringBuilder hql = null;
-			Query qo = null;
-			try {
-				
-			hql.append(" UPDATE Student s"); 
-			hql.append(" SET s.identification:identificacionStudent,"); 
+
+			hql.append(" UPDATE Student s");
+			hql.append(" SET s.identification:identificacionStudent,");
 			hql.append("s.name:nameStudent, ");
 			hql.append("s.idCourse:idCourse");
 			hql.append(" WHERE s.id:idStudent");
 			hql.append(" AND s.state:state ");
-			
-			qo = getSession().createQuery(hql.toString()).setResultTransformer(Transformers.aliasToBean(Student.class));
+
+			qo = getSession().createQuery(hql.toString()).setResultTransformer(
+					Transformers.aliasToBean(Student.class));
 			qo.setParameter("idStudent", idStudent);
 			qo.setParameter("identificacionStudent", identificacionStudent);
 			qo.setParameter("nameStudent", nameStudent);
 			qo.setParameter("idCourse", idCourse);
 			qo.setParameter("state", IState.ACTIVE);
 
-			
 			qo.executeUpdate();
 			return true;
-			
-			} catch (Exception e) {
-				throw e;
-			}finally{
-				hql = null;
-				qo = null;
-			}
-	
-		}}
-	
+
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			hql = null;
+			qo = null;
+		}
+
+	}
+
+}
