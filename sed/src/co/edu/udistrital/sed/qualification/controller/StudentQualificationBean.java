6893b2524d12f3e12f2bf6cd9b27ab930610ac47
migueltrock <migueltrock@gmail.com>
2014-06-04 03:03:51 +0000

    enviando mejoras en header y login

--- sed/src/co/edu/udistrital/sed/qualification/controller/StudentQualificationBean.java
+++ sed/src/co/edu/udistrital/sed/qualification/controller/StudentQualificationBean.java
@@ -1,25 +1,34 @@
 package co.edu.udistrital.sed.qualification.controller;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
 import com.ocpsoft.pretty.faces.annotation.URLMapping;
 
 import co.edu.udistrital.core.common.controller.BackingBean;
+import co.edu.udistrital.sed.model.Qualification;
 
 @ManagedBean
 @ViewScoped
 @URLMapping(id = "stQualification", pattern = "/portal/calificaciones", viewId = "/pages/student/qualification/qualification.jspx")
 public class StudentQualificationBean extends BackingBean {
 	
+	//Primitives
 	private int activeIndex;
+	
+	//User List
+	private List<Qualification>studentQualificationList;
 
 	// Controller Object
 	private StudentQualificationController controller;
 
-	public StudentQualificationBean() {
+	public StudentQualificationBean() throws Exception {
 		try {
 			this.controller = new StudentQualificationController();
+				this.studentQualificationList = this.controller.loadStudentQualificationList(getUserSession().getId(),getUserSession().getIdSedRole());
+				
 		} catch (Exception e) {
 			throw e;
 		}
@@ -44,6 +53,14 @@ public class StudentQualificationBean extends BackingBean {
 	public void setActiveIndex(int activeIndex) {
 		this.activeIndex = activeIndex;
 	}
-	
+
+	public List<Qualification> getStudentQualificationList() {
+		return studentQualificationList;
+	}
+
+	public void setStudentQualificationList(List<Qualification> studentQualificationList) {
+		this.studentQualificationList = studentQualificationList;
+	}
+		
 
 }
--- sed/src/co/edu/udistrital/sed/qualification/controller/StudentQualificationController.java
+++ sed/src/co/edu/udistrital/sed/qualification/controller/StudentQualificationController.java
@@ -1,7 +1,31 @@
 package co.edu.udistrital.sed.qualification.controller;
 
-import co.edu.udistrital.core.common.controller.Controller;
+import java.util.List;
 
-public class StudentQualificationController extends Controller{
+import org.hibernate.Transaction;
+
+import co.edu.udistrital.core.common.controller.Controller;
+import co.edu.udistrital.sed.model.Qualification;
+import co.edu.udistrital.sed.model.QualificationDAO;
+
+public class StudentQualificationController extends Controller {
+
+	/** @author MTorres */
+	public List<Qualification> loadStudentQualificationList(Long idSedUser, Long idSedRole) throws Exception {
+		QualificationDAO dao = new QualificationDAO();
+		Transaction tx = null;
+		try {
+			tx = dao.getSession().beginTransaction();
+			return dao.loadStudentQualificationList(idSedUser, idSedRole);
+		} catch (Exception e) {
+			dao.getSession().cancelQuery();
+			tx.rollback();
+			throw e;
+		} finally {
+			dao.getSession().close();
+			dao = null;
+			tx = null;
+		}
+	}
 
 }
