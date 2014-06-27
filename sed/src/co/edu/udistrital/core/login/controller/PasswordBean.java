7dd7f635f86f34e51b7517fe273fc308c7a775bc
migueltrock <migueltrock@gmail.com>
2014-06-27 02:54:36 +0000

    Enviando adelanto cambio de contraseña

--- sed/src/co/edu/udistrital/core/login/controller/PasswordBean.java
+++ sed/src/co/edu/udistrital/core/login/controller/PasswordBean.java
@@ -3,29 +3,113 @@ package co.edu.udistrital.core.login.controller;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
+import org.hibernate.Transaction;
+
 import com.ocpsoft.pretty.faces.annotation.URLMapping;
 
 import co.edu.udistrital.core.common.controller.BackingBean;
+import co.edu.udistrital.core.common.encryption.ManageMD5;
+import co.edu.udistrital.core.common.util.FieldValidator;
+import co.edu.udistrital.core.common.util.RandomPassword;
+import co.edu.udistrital.core.login.model.SedUser;
+import co.edu.udistrital.core.login.model.SedUserDAO;
 
 @ManagedBean
 @ViewScoped
 @URLMapping(id = "password", pattern = "/portal/clave", viewId = "/pages/password/password.jspx")
 public class PasswordBean extends BackingBean {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 9075256738266226834L;
 	private PasswordController controller;
+	private String newPass;
+	private String password;
+	private String newPass2;
 
 	public PasswordBean() {
 		try {
 			this.controller = new PasswordController();
+			getUserSession().getIdSedUser();
 		} catch (Exception e) {
 			throw e;
 		}
 	}
 
+	public boolean isRightNewPass() {
+		try {
+			if (this.newPass == null || this.newPass.trim().isEmpty()) {
+				addWarnMessage("Valida",
+						"Por favor ingrese la nueva contraseña");
+				return false;
+			} else if (this.newPass2 == null || this.newPass2.trim().isEmpty()) {
+				addWarnMessage("Valida", "Por favor ingrese la ");
+				return false;
+			} else if (!this.newPass.equals(this.newPass2)) {
+				addWarnMessage("Valida", "Por favor ingrese la ");
+				return false;
+			}
+
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+	}
+
+	public void changePassword() {
+		try {
+			if (!isRightNewPass())
+				return;
+			if (this.controller.rightOldPass(getUserSession().getIdSedUser(),
+					this.password)) {
+				this.controller.updateSedUserPassword(getUserSession()
+						.getIdSedUser(), this.newPass);
+				BackingBean.addInfoMessage("Restablecer contraseña",
+						"Cambio de contraseña exitoso.");
+
+			} else {
+
+				BackingBean.addInfoMessage("Clave Antigua Incorrecta",
+						"Por favor ingrese la clave correcta.");
+				return;
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 	public boolean getValidateSedUserRole() throws Exception {
+		String md5Pass = ManageMD5.parseMD5("123456");
 
 		return true;
 	}
 
+	public String getNewPass() {
+		return newPass;
+	}
+
+	public void setNewPass(String newPass) {
+		this.newPass = newPass;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getNewPass2() {
+		return newPass2;
+	}
+
+	public void setNewPass2(String newPass2) {
+		this.newPass2 = newPass2;
+	}
+	
 
 }
--- sed/src/co/edu/udistrital/core/login/controller/PasswordController.java
+++ sed/src/co/edu/udistrital/core/login/controller/PasswordController.java
@@ -1,7 +1,56 @@
 package co.edu.udistrital.core.login.controller;
 
+import org.hibernate.Transaction;
+
 import co.edu.udistrital.core.common.controller.Controller;
+import co.edu.udistrital.core.login.model.SedUserDAO;
 
 public class PasswordController extends Controller{
-
+	
+	private static final long serialVersionUID = -2401144292039751003L;
+	
+	public boolean   rightOldPass(Long idUser,String password) {
+		SedUserDAO dao = new SedUserDAO();
+		Transaction tx = null;
+		boolean success= false;
+		
+		try {
+			
+	tx = dao.getSession().beginTransaction();
+	success= dao.validateOldPassword(idUser, password);
+	tx.commit();
+	
+	
+	
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return success;
+	}
+	
+	
+	public boolean updateSedUserPassword(Long idSedUser, String password) throws Exception {
+		SedUserDAO dao = new SedUserDAO();
+		Transaction tx = null;
+		boolean success = false;
+		try {
+			if	(dao.validateOldPassword(idSedUser, password))
+			{
+			tx = dao.getSession().beginTransaction();
+			success = dao.updateSedUserPassword(idSedUser, password);
+			tx.commit();
+			success = true;
+			}
+		} catch (Exception e) {
+			dao.getSession().cancelQuery();
+			tx.rollback();
+			throw e;
+		} finally {
+			dao.getSession().close();
+			dao = null;
+			tx = null;
+		}
+		return success;
+	}
 }
