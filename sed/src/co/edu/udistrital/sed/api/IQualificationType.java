c084b44d24b6b4e40b190f02f45e450e3469c72a
migueltrock <migueltrock@gmail.com>
2014-09-16 03:18:01 +0000

    Enviando Reporte final, verificación con Patricia

--- sed/src/co/edu/udistrital/sed/api/IQualificationType.java
+++ sed/src/co/edu/udistrital/sed/api/IQualificationType.java
@@ -5,6 +5,7 @@ public interface IQualificationType {
 	public static Long C1 = Long.valueOf(1);
 	public static Long C2 = Long.valueOf(2);
 	public static Long C3 = Long.valueOf(3);
-	public static Long CF = Long.valueOf(4);
+	public static Long C4 = Long.valueOf(4);
+	public static Long CF = Long.valueOf(5);
 
 }
